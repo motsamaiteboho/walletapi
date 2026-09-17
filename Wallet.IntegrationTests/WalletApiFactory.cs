949e62d4306ddad64879b22a2a62b292aab37ad0
Teboho <motsamaiteboho@yahoo.com>
2026-09-17 23:20:20 +0200

    feat: complete wallet withdrawal vertical slice

---
+++ Wallet.IntegrationTests/WalletApiFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace Wallet.IntegrationTests
+{
+    public class WalletApiFactory
+    : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(
+            IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(
+                (_, config) =>
+                {
+                    var settings = new Dictionary<string, string?>
+                    {
+                        ["ConnectionStrings:WalletDatabase"] =
+                            "Host=localhost;" +
+                            "Port=5433;" +
+                            "Database=wallet;" +
+                            "Username=postgres;" +
+                            "Password=postgres"
+                    };
+
+                    config.AddInMemoryCollection(settings);
+                });
+        }
+    }
+}
---
+++ Wallet.IntegrationTests/WalletApiTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using Wallet.Application.Features.WalletAccounts.Withdraw;
+
+namespace Wallet.IntegrationTests
+{
+    public class WalletApiTests
+    {
+        [Fact]
+        public async Task GetBalance_WithExistingWallet_ReturnsOk()
+        {
+            // Arrange
+            await using var factory = new WalletApiFactory();
+
+            var client = factory.CreateClient();
+
+            var walletId =
+                Guid.Parse(
+                    "11111111-1111-1111-1111-111111111111");
+
+            // Act
+            var response = await client.GetAsync(
+                $"/api/wallet-accounts/{walletId}/balance");
+
+            // Assert
+            Assert.Equal(
+                HttpStatusCode.OK,
+                response.StatusCode);
+        }
+    }
+}
