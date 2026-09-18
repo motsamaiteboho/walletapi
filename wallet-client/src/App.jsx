e6aa303f0f7595c199e7fbb6aeb09affb09ee1fc
Teboho <motsamaiteboho@yahoo.com>
2026-09-18 16:30:04 +0200

    feat: initialize react wallet client

---
+++ wallet-client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <h1>My Wallet</h1>
+            <p>Welcome to your wallet.</p>
+        </div>
+    );
+}
+
+export default App;
